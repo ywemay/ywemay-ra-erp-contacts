c4f917fcff4acab726fd3e2c6f6aa8d3d43af886
Dorian Chibuc <devel@ywemay.com>
2024-05-15 17:58:45 +0800

    Fixes

--- inputs/Val.jsx
+++ inputs/Val.jsx
@@ -1,18 +1,14 @@
 import { TextInput, required, email } from "react-admin";
-import { useWatch } from "react-hook-form";
 import { getLabel } from "../utils/getLabel";
 
 export default function ValInput(props) {
-  const contactType = useWatch({ name: "contact_type" });
-  const label = getLabel({contactType})
 
    const validate = [
     required(),
-    contactType === 1 ? email() : undefined,
   ];
 
   return <TextInput
-    label={label}
+    label="value.value"
     source="val"
     validate={validate}
     {...props} />
--- resource.js
+++ resource.js
@@ -1,4 +1,6 @@
 export const resource='contacts';
+import { Edit as edit } from './edit/Edit'
+import { Create as create } from './edit/Create'
 
 export default {
   name: 'contacts',
